8ad637eea1449772477b9cc40b1f56c51cf2c3c2
beatricebraghieri2008 <braghieri.beatrice@iisgalvanimi.edu.it>
2026-05-26 08:52:25 +0000

    Completato Frontend Feature B: form Autori e Prestiti senza errori

--- frontend/src/app/app-module.ts
+++ frontend/src/app/app-module.ts
@@ -1,15 +1,25 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { provideHttpClient } from '@angular/common/http'; // 👈 Usiamo questo che è moderno e facilissimo!
 
-import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import { AggiungiAutore } from './components/aggiungi-autore/aggiungi-autore';
-import { RegistraPrestito } from './components/registra-prestito/registra-prestito';
+import { AggiungiAutoreComponent } from './components/aggiungi-autore/aggiungi-autore';
+import { RegistraPrestitoComponent } from './components/registra-prestito/registra-prestito';
 
 @NgModule({
-  declarations: [App, AggiungiAutore, RegistraPrestito],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [provideBrowserGlobalErrorListeners()],
-  bootstrap: [App],
+  declarations: [
+    App,
+    AggiungiAutoreComponent,
+    RegistraPrestitoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule // 👈 Qui dentro ora c'è solo questo, super pulito!
+  ],
+  providers: [
+    provideHttpClient() // 👈 Diciamo ad Angular di attivare internet da qui
+  ],
+  bootstrap: [App]
 })
-export class AppModule {}
+export class AppModule { }
--- frontend/src/app/components/registra-prestito/registra-prestito.ts
+++ frontend/src/app/components/registra-prestito/registra-prestito.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { BibliotecaService } from '../../services/biblioteca';
 
 @Component({
   selector: 'app-registra-prestito',
-  standalone: false,
-  templateUrl: './registra-prestito.html',
-  styleUrl: './registra-prestito.css',
+  templateUrl: './registra-prestito.component.html',
+  styleUrls: ['./registra-prestito.component.css']
 })
-export class RegistraPrestito {}
+export class RegistraPrestitoComponent {
+  // Oggetto che conterrà i dati del prestito scritti nel form
+  nuovoPrestito = {
+    Data_Inizio: '',
+    Data_Scadenza: '',
+    id_copie_fk: null, // ID della copia del libro preso in prestito
+    id_utenti_fk: null  // ID dell'utente che prende il libro
+  };
+
+  constructor(private bibliotecaService: BibliotecaService) {}
+
+  // Funzione che si attiva quando l'utente preme il bottone "Registra"
+  inviaForm() {
+    this.bibliotecaService.aggiungiPrestito(this.nuovoPrestito).subscribe({
+      next: (risposta) => {
+        alert('Prestito registrato con successo nel database!');
+        // Resetta il form
+        this.nuovoPrestito = { Data_Inizio: '', Data_Scadenza: '', id_copie_fk: null, id_utenti_fk: null };
+      },
+      error: (errore) => {
+        console.error(errore);
+        alert("Errore durante la registrazione del prestito.");
+      }
+    });
+  }
+}
